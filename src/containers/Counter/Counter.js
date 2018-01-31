72a1b122f2c8a140c81528d800fca91b7f11d8f4
mitsunori.morita <mitsunori.morita@speee.jpmitsunori.morita@speee.jpmitsunori.morita@speee.jp>
2018-01-31 19:54:47 +0900

    results state add

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -5,7 +5,8 @@ import CounterOutput from '../../components/CounterOutput/CounterOutput';
 
 class Counter extends Component {
     state = {
-        counter: 0
+        counter: 0,
+
     }
 
     counterChangedHandler = ( action, value ) => {
@@ -33,14 +34,24 @@ class Counter extends Component {
                 <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
                 <CounterControl label="Add 10" clicked={this.props.onAddCounter}  />
                 <CounterControl label="Subtract 15" clicked={this.props.onSubtractCounter}  />
-            </div>
+              <hr/>
+
+              <button onClick={this.props.onStoreResult}>Store Result</button>
+              <ul>
+                {this.props.storedResults.map(strResult => (
+                  <li key={strResult.id} onClick={this.props.onDeleteResult}>{strResult.value}</li>
+                ))}
+              </ul>
+
+          </div>
         );
     }
 }
 
 const mapStateToProps = state => {
   return {
-    ctr: state.counter
+    ctr: state.counter,
+    storedResults: state.results
   };
 };
 
@@ -50,7 +61,9 @@ const mapDispatchToProps = dispatch => {
     onIncrementCounter: () => dispatch({type: 'INCREMENT'}),
     onDecrementCounter: () => dispatch({type: 'DECREMENT'}),
     onAddCounter: () => dispatch({type: 'ADD', val: 10 }),
-    onSubtractCounter: () => dispatch({type: 'SUBTRACT', val: 15})
+    onSubtractCounter: () => dispatch({type: 'SUBTRACT', val: 15}),
+    onStoreResult: () => dispatch({type: 'STORE_RESULT'}),
+    onDeleteResult: () => dispatch({type: 'DELETE_RESULT'})
   };
 };
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,54 +1,37 @@
 
 const initialState = {
-  counter: 0
+  counter: 0,
+  results: []
 }
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case 'INCREMENT':
-      return {
-        counter: state.counter + 1
-      }
+      const newState = Object.assign({}, state);
+      newState.counter = state.counter + 1;
+      return newState;
     case 'DECREMENT':
       return {
+        ...state,
         counter: state.counter - 1
       }
     case 'ADD':
       return {
+        ...state,
         counter: state.counter + action.val
       }
     case 'SUBTRACT':
       return {
+        ...state,
         counter: state.counter - action.val
-    }
-    default:
-
+      }
+    case 'STORE_RESULT':
+      return {
+        ...state,
+        results: state.results.concat({id: new Date() , value: state.counter})
+      }
   }
   return state;
 };
-//   if(action.type === 'INCREMENT'){
-//     return {
-//       counter: state.counter + 1
-//     }
-//   }
-//   if(action.type === 'DECREMENT'){
-//     return {
-//       counter: state.counter - 1
-//     }
-//   }
-//   if(action.type === 'ADD'){
-//     return {
-//       //actionによるデータの受け渡し プロパティ名で記述
-//       counter: state.counter + action.val
-//     }
-//   }
-//   if(action.type === 'SUBTRACT'){
-//     return {
-//       counter: state.counter - action.val
-//     }
-//   }
-//   return state;
-// }
-
 
 export default reducer;
